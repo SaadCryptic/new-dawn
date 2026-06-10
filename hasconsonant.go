32fe5dcce3181db5b48ec143c004aa2cc3b9d6e3
SaadCryptic <olarewajujamiu832@gmail.com>
2026-06-10 02:40:36 +0100

    beginning

--- hasconsonant.go
+++ hasconsonant.go
@@ -1,6 +1,4 @@
 package main
-
-import "fmt"
 func HasVowel(char rune) bool {
 	return (char == 'A' || char == 'E' || char == 'I' || char == 'O' ||char == 'U') ||
 		(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u')
@@ -12,9 +10,6 @@ func HasConsonant (str string) bool {
 		}
 	}
 	return false
-}
-func main (){
-	fmt.Println(HasConsonant("aou"))
 }
 // this is great but if we are to pass a special case into the tester function, it will return true whch is not what we want.
 //Create another file that correct this.
